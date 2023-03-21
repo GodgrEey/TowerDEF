fb9690dd88f7f18332d04004c6d25316525da640
GodgrEey <jremy.vdb@gmail.com>
2023-03-21 14:50:40 +0100

    ezwin

--- Assets/Main_menu/Scripts/OptionsScreen.cs
+++ Assets/Main_menu/Scripts/OptionsScreen.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.UI;
 public class OptionsScreen : MonoBehaviour
 {
+    public AudioMixer TheMixer;
+    public TMP_Text mastLabel, musicLabel, sfxLabel;
+    public Slider mastSlider, musicSlider, sfxSlider;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,30 @@ public class OptionsScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    public void SetMasterVol()
+    {
+    mastLabel.text = Mathf.RoundToInt(mastSlider.value + 80).ToString();
+
+    TheMixer.SetFloat("MasterVol", mastSlider.value);
+    
+    }
+   
+   
+    public void SetMusicVol()
+    {
+    musicLabel.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
+
+    TheMixer.SetFloat("MusicVol", musicSlider.value);
+    
+    }
+
+
+    public void SetSFXVol()
+    {
+    sfxLabel.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
+
+    TheMixer.SetFloat("SFXVol", sfxSlider.value);
+    
     }
 }
